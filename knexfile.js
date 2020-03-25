47ef3dbfb215882edfec840c03c333769d327c94
Eric Karnis <ekarnis@outlook.com>
2020-03-24 20:16:57 -0400

    changed db connection process

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,7 @@
 module.exports = {
   development: {
     client: 'postgresql',
-    connection: {
-      host: '127.0.0.1',
-      user: 'postgres',
-      password: '',
-      database: 'ecommerce'
-    },
+    connection: process.env.PG_CONNECTION_STRING,
     migrations: {
       tableName: 'knex_migrations'
     }
