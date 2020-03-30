476895810951937b02816895967d424e3d52244b
farazoman <farazoman@gmail.com>
2020-03-29 22:57:50 -0400

    readded the Link for the boards, throws an error though for updating state of an unmounted component

--- components/InStockBox.tsx
+++ components/InStockBox.tsx
@@ -34,7 +34,6 @@ export default props => {
   }
 
   return (
-    <a className="w-64 flex flex-col items-center p-4 m-4 border border-gray-200 hover:border-gray-400 bg-white rounded-lg shadow-lg hover:shadow-xl">
         <Link
           href="/boards/[id]"
           as={
@@ -42,15 +41,17 @@ export default props => {
             props.id
           }
         >
+      <a className="w-64 flex flex-col items-center p-4 m-4 border border-gray-200 hover:border-gray-400 bg-white rounded-lg shadow-lg hover:shadow-xl">
           <h3 className="text-3xl font-bold text-indigo-500">{props.wood.name}</h3>
           <span>
             {props.width_in_cm}cm x {props.length_in_cm}cm x {props.thickness_in_cm}cm
           </span>
           <span className="mb-4">{props.stain.name}</span>
           <img src={props.picture_url}></img>
-        </Link>
         <PrimaryButton onClick={e => onCartClick() } buttonText={'Add to Cart'} />
         <Toast isAdded={isAdded} toastMsg={toastMsg}/>
       </a>
+        </Link>
+      
   )
 }
