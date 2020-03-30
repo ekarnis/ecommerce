71fd3a3376395efff64a961e82c97ec6ed12c479
farazoman <farazoman@gmail.com>
2020-03-29 22:17:20 -0400

    readded the link to the specific board page

--- components/InStockBox.tsx
+++ components/InStockBox.tsx
@@ -34,19 +34,23 @@ export default props => {
   }
 
   return (
-    <Link
-      href="/in-stock"
-    >
-      <a className="w-64 flex flex-col items-center p-4 m-4 border border-gray-200 hover:border-gray-400 bg-white rounded-lg shadow-lg hover:shadow-xl">
-        <h3 className="text-3xl font-bold text-indigo-500">{props.wood.name}</h3>
-        <span>
-          {props.width_in_cm}cm x {props.length_in_cm}cm x {props.thickness_in_cm}cm
-        </span>
-        <span className="mb-4">{props.stain.name}</span>
-        <img className="my-2" src={props.picture_url}></img>  
+    <a className="w-64 flex flex-col items-center p-4 m-4 border border-gray-200 hover:border-gray-400 bg-white rounded-lg shadow-lg hover:shadow-xl">
+        <Link
+          href="/boards/[id]"
+          as={
+            '/boards/' +
+            props.id
+          }
+        >
+          <h3 className="text-3xl font-bold text-indigo-500">{props.wood.name}</h3>
+          <span>
+            {props.width_in_cm}cm x {props.length_in_cm}cm x {props.thickness_in_cm}cm
+          </span>
+          <span className="mb-4">{props.stain.name}</span>
+          <img src={props.picture_url}></img>
+        </Link>
         <PrimaryButton onClick={e => onCartClick() } buttonText={'Add to Cart'} />
         <Toast isAdded={isAdded} toastMsg={toastMsg}/>
       </a>
-    </Link>
   )
 }
