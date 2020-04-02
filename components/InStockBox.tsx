182c9e6d84fd93a57b74d27e248f5042dec0296b
farazoman <farazoman@gmail.com>
2020-04-02 00:35:05 -0400

    remvoed Link tag from the addto order button element

--- components/InStockBox.tsx
+++ components/InStockBox.tsx
@@ -39,23 +39,25 @@ export default props => {
   }
 
   return (
-    <Link
-      href="/boards/[id]"
-      as={
-        '/boards/' +
+    <a className="w-64 flex flex-col items-center p-4 m-4 border border-gray-200 hover:border-gray-400 bg-white rounded-lg shadow-lg hover:shadow-xl">
+      <Link
+        href="/boards/[id]"
+        as={
+          '/boards/' +
             props.id
-      }
-    >
-      <a className="w-64 flex flex-col items-center p-4 m-4 border border-gray-200 hover:border-gray-400 bg-white rounded-lg shadow-lg hover:shadow-xl">
-        <h3 className="text-3xl font-bold text-indigo-500">{props.wood.name}</h3>
-        <span>
-          {props.width_in_cm}cm x {props.length_in_cm}cm x {props.thickness_in_cm}cm
-        </span>
-        <span className="mb-4">{props.stain.name}</span>
-        <img src={props.picture_url}></img>
-        <PrimaryButton onClick={e => onCartClick() } buttonText={'Add to Cart'} />
-        <Toast isVisible={isVisible} toastMessage={toastMessage} toastType={toastType}/>
-      </a>
-    </Link>
+        }
+      >
+        <div>
+          <h3 className="text-3xl font-bold text-indigo-500">{props.wood.name}</h3>
+          <span>
+            {props.width_in_cm}cm x {props.length_in_cm}cm x {props.thickness_in_cm}cm
+          </span>
+          <span className="mb-4">{props.stain.name}</span>
+          <img src={props.picture_url}></img>
+        </div>
+      </Link>
+      <PrimaryButton onClick={e => onCartClick() } buttonText={'Add to Cart'} />
+      <Toast isVisible={isVisible} toastMessage={toastMessage} toastType={toastType}/>
+    </a>
   )
 }
